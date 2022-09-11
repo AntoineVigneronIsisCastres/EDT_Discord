8e962e8cbf2ce1444347a84462d9869326ef6ed4
AntoineVigneronIsisCastres <98307921+AntoineVigneronIsisCastres@users.noreply.github.com>
2022-09-12 00:07:25 +0200

    Update edt_discord.py

--- edt_discord.py
+++ edt_discord.py
@@ -52,7 +52,7 @@ def get_page(url, promo, semainepro):
     cssheure = "[style*='top: 91px;']"
     mardi = []
     for matiere in mardimatieres:
-        matiereparse = BeautifulSoup(matiere, 'html.parser')
+        matiereparse = BeautifulSoup(matiere.get_attribute('outerHTML'), 'html.parser')
         print(matiereparse)
         set_matiere_heure(driver, cssheure, matiereparse, mardi)
         mardi.append(matiereparse)
