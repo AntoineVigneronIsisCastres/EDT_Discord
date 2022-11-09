1c99422d3d9a1fa2a0fdce2c5d4bb6b60e495d9e
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2022-11-09 10:25:09 +0100

    fix

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def seconds_until(hours, minutes):
 async def daily_edt():
     while True:
         await asyncio.sleep(seconds_until(23,00))
-        edt_discord.edt_daily()
+        #edt_discord.edt_daily()
         print("OH LE WEBHOOK IL MARCHE PAS IL EST "+str(datetime.datetime.now()))
         webhook = DiscordWebhook(url=urlWebhook, username="ISIS",
                              avatar_url="https://media.discordapp.net/attachments/848264360119238706/849298764530974760/webhook_2_2.png")
