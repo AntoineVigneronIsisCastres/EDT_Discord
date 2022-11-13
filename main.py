9e5ac912c7105703e18fbff9f539da40c416162c
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2022-11-13 19:21:15 +0100

    Fixed wrong daily hour query

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def seconds_until(hours, minutes):
 @tasks.loop(hours=24)
 async def daily_edt():
     while True:
-        await asyncio.sleep(seconds_until(23,00))
+        await asyncio.sleep(seconds_until(6,00))
         edt_discord.edt_daily()
         await asyncio.sleep(60)
 
