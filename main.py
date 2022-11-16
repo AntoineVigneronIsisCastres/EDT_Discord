3e39af5ff9cfe962e18ff20ff1c5ffc741b6f979
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2022-11-16 08:45:44 +0100

    Set hour of daily fetch at 6:30 pm UTC+1

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def seconds_until(hours, minutes):
 @tasks.loop(hours=24)
 async def daily_edt():
     while True:
-        await asyncio.sleep(seconds_until(7,40))
+        await asyncio.sleep(seconds_until(5,30))
         edt_discord.run_daily()
         await asyncio.sleep(60)
 
